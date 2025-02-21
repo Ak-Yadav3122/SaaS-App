40575e136aef83004e9242a1716fb5b3467048f7
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-02-21 19:48:16 +0530

    Update the hoem page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,18 @@
-import Navbar from "@/components/home/navbar/navbar";
-import { Button } from "@/components/ui/button";
+import Navbar from "@/components/home/header/navbar";
+import HeroSection from "@/components/home/heroSection";
+import PricingPage from "@/components/home/PricingPage";
+import Footer from "@/components/home/footerpage";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
+      <div className="grid items-center justify-items-center min-h-screen p-8 gap-16 sm:p-20">
+        <HeroSection totalForms={0} isSubscribed={false} />
+        <PricingPage />
+        <Footer />
+      </div>
     </div>
   );
 };
