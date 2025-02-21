03a2e6bede242600b9c67ca4c9178765379615eb
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-02-21 19:49:38 +0530

    generate form input area

---
+++ components/home/generateFormInput.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+const GenerateFormInput = () => {
+  return (
+    <div className="flex items-center gap-4 my-8">
+      <Input type="test" placeholder="write a prompt to generate the form" />
+      <Button>Generate Form</Button>
+    </div>
+  );
+};
+
+export default GenerateFormInput;
