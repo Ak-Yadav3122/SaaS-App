6ae738dc753699d6fe4869028d87a770d4ccb087
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-02-22 18:24:38 +0530

    adding page for dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+"use server"
+import DashboardSidebar from '@/components/dashboard/sidebar'
+import React from 'react'
+
+const DashBoardPage = () => {
+  return (
+    <div>
+      <DashboardSidebar />
+      {/* <h1>hello jee</h1> */}
+    </div>
+  )
+}
+
+export default DashBoardPage
