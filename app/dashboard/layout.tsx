0d0bb8b6908cb9975c79628bc2764cd91202b712
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-02-22 18:24:23 +0530

    addin layput of dashboard

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+ 
+import DashboardSidebar from "@/components/dashboard/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import React from "react";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <SidebarProvider>
+      <DashboardSidebar />
+      <main className="mx-6 my-4 w-full">{children}</main>
+    </SidebarProvider>
+  );
+};
+
+export default layout;
