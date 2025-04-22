28ebfe5253519f44aae4c95d1d0024ddc839a45f
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-04-22 19:51:46 +0530

    update the not found message

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const pageNotFound = () => {
   return (
     <div className="h-screen w-screen  flex items-center justify-center">
-      <h1 className="font-bold text-blue-600 text-2xl">Page are not found...</h1>
+      <h1 className="font-bold text-blue-600 text-2xl">Oops! The page was not found...</h1>
     </div>
   );
 };
