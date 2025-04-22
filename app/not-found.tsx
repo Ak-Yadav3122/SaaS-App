f38d7af7a6cb7add93ec42305f29a662638f349f
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-04-22 19:53:28 +0530

    add the home route on not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,6 +5,7 @@ const pageNotFound = () => {
   return (
     <div className="h-screen w-screen  flex items-center justify-center">
       <h1 className="font-bold text-blue-600 text-2xl">Oops! The page was not found...</h1>
+      <a href="/" className="mt-6 text-blue-600 hover:underline">Go Back Home</a>
     </div>
   );
 };
